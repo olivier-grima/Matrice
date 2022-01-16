f454f7fea4645c7ef8bf3b7b8e59aa2b12b943dd
Olivier <olivier.grima@etu.univ-cotedazur.fr>
2022-01-16 19:05:09 +0100

    comments

--- matriceDouble.hpp
+++ matriceDouble.hpp
@@ -18,19 +18,14 @@ class matriceDouble:public Matrice <double>
         //destructeur virtuel -> on supprime la matrice
         virtual ~matriceDouble();
 
-
         virtual double get(const int i, const int j)const override;
-        
         virtual void set(const double &x,const int i, const int j) override;
 
         virtual string toString() const override;
-        
+
         virtual Matrice<double>* subMat(int i1,int i2, int j1,int j2)const override;
-        
         virtual Matrice<double>* SomMat(const Matrice<double> &m1)const override;
-
         virtual Matrice<double>* MultMat(const Matrice<double> &m1)const override;
-
         //methode de conversion d'une matriceDouble a une matriceCreuse
         virtual Matrice<double>* MDtoMC()const override;
         //methode de conversion d'une matriceCreuse a une matriceDouble
@@ -38,9 +33,4 @@ class matriceDouble:public Matrice <double>
         //methode de test de matrice, on compte le nombre de 0, si il est superieur a 90% de la matrice, elle est creuse
         virtual int estCreuse()const override;
         virtual int NbrElementNN()const override;
-
-
-       
-      
-
 };
